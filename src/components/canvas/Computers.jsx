b93e10b05e37a02f70d76c38dd2de3332525e318
Rohan1711 <rohankakare@gmail.com>
2024-01-19 14:43:59 +0530

    Bug fixed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -58,7 +58,7 @@ const ComputersCanvas = () => {
       frameloop='demand'
       shadows
       dpr={[1, 2]}
-      camera={{ position: [20, 3, 5], fov: 25 }}
+      camera={{ position: [20, isMobile ? 5 : 3, 5], fov: isMobile ? 40 : 25}}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
